9bbffbe22149e36a8a2de0b07db4f996edd65ffd
bingo5630 <binghh5@gmail.com>
2025-11-21 17:17:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -65,8 +65,8 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 #Short Url or Api
-SHORT_URL = os.environ.get("SHORTNER_URL", "linkshortify.com")
-SHORT_API = os.environ.get("SHORTNER_API", "7a541402aeacbdbff361696add89bce2cf8afa4e")
+SHORT_URL = os.environ.get("SHORTNER_URL", "modishort.com")
+SHORT_API = os.environ.get("SHORTNER_API", "08cb7a31ae7535f052784ee72614e870d60525a3")
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "Pʟᴇᴀꜱᴇ ᴅᴏɴ'ᴛ ᴍᴇꜱꜱᴀɢᴇ ᴍᴇ ᴅɪʀᴇᴄᴛʟʏ ɪ ᴀᴍ ᴏɴʟʏ ᴡᴏʀᴋ ꜰᴏʀ - [@Cultured_Aodox]"
